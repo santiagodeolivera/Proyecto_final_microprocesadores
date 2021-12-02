c3a6a3dc6414e015bad420179df13a98ed28d55c
Santiago De Olivera <santiago.deolivera@correo.ucu.edu.uy>
2021-12-02 09:29:25 -0300

    Reorganize

--- receiver/src/main.asm
+++ receiver/src/main.asm
@@ -2,15 +2,15 @@
 	JMP start
 .ORG 0x0016
 	JMP tmr1_start
-.ORG 0x001C
-	JMP tmr0_start
+.ORG 0x0024
+	JMP usartR_start
 
 .INCLUDE "lib.asm"
 
 #define BUFFER_SIZE 2
 
 .DSEG
-	data_buffer: .BYTE BUFFER_SIZE + 2
+	data_buffer: .BYTE BUFFER_SIZE
 
 	shield_buffer: .BYTE 4
 	shield_digits: .BYTE 16
@@ -58,22 +58,10 @@ start:
 	STZ 0b00100010
 	STZ 0b00010001
 	
-	SETZ tmr1_hamming_buffer
-	STZ 0b11111111
-
-	SETZ tmr1_state
-	STZ 0
-
-	SETZ tmr1_counter
+	SETZ usartR_counter
 	STZ 0
 	STZ 0
 
-	SETZ tmr1_checksum
-	STZ 0
-	STZ 0
-
-	SETZ pseudorand_mem
-	STZ -1
 
 	TIMER0SETUP 50
 	TIMER1SETUP 100
@@ -116,149 +104,23 @@ program:
 
 // timer 1 interruption
 .DSEG
-	tmr1_state: .BYTE 1
-	tmr1_counter: .BYTE 2
-	tmr1_hamming_buffer: .BYTE 1
-	tmr1_checksum: .BYTE 2
+	usartR_counter: .BYTE 2
 .CSEG
-tmr1_start:
+usartR_start:
 	PUSH r0
 	IN r0, SREG
 	PUSH r0
 	PUSH r16
 	PUSH r17
 
-	LDS r17, tmr1_state
-	CPI r17, 0
-	BREQ tmr1_call_write_data
-
-	CALL send_data
-	RJMP tmr1_end
-
-	tmr1_call_write_data:
-		CALL write_data
 	
-tmr1_end:
+usartR_end:
 	POP r17
 	POP r16
 	POP r0
 	OUT SREG, r0
 	POP r0
 	RETI
-
-// void send_data();
-write_data:
-	PUSH r16
-	PUSH r17
-	PUSH r18
-	PUSH r19
-	PUSH r20
-	PUSH r21
-
-	CALL generate_pseudorandom_number
-	MOV r17, r16
-
-	LDS r18, tmr1_counter + 0
-	LDS r19, tmr1_counter + 1
-
-	SETZ data_buffer
-	SUMZW r18, r19
-	ST Z, r17
-
-	LDS r20, tmr1_checksum + 0
-	LDS r21, tmr1_checksum + 1
-	ADDW1 r20, r21, r17
-	STS tmr1_checksum + 0, r20
-	STS tmr1_checksum + 1, r21
-
-	INCW r18, r19
-	STS tmr1_counter + 0, r18
-	STS tmr1_counter + 1, r19
-
-	CPWI r18, r19, BUFFER_SIZE
-	BRNE writedata_end
-	
-	STS data_buffer + BUFFER_SIZE + 0, r20
-	STS data_buffer + BUFFER_SIZE + 1, r21
-
-	LDI r21, 1
-	STS tmr1_state, r21
-
-	LDI r21, 0
-	STS tmr1_counter + 0, r21
-	STS tmr1_counter + 1, r21
-
-writedata_end:
-	POP r21
-	POP r20
-	POP r19
-	POP r18
-	POP r17
-	POP r16
-	RET
-
-// void send_data();
-send_data:
-	PUSH r16
-	PUSH r17
-	PUSH r18
-	PUSH r19
-	PUSH r20
-
-	LDS r17, tmr1_counter + 0
-	LDS r18, tmr1_counter + 1
-
-	CPWI r17, r18, BUFFER_SIZE + 2
-	BRSH senddata_end0
-	RJMP senddata_continue0
-
-	senddata_end0:
-		JMP senddata_end
-	senddata_continue0:
-
-	CALL can_write
-	CPI r16, 0
-	BREQ senddata_end0
-
-	LDS r19, tmr1_hamming_buffer
-	MOV r20, r19
-	ORI r19, 0b01111111
-	CPI r19, 0xFF
-	BREQ senddata_write_normal
-		LSL r20
-		STS UDR0, r20
-		LDI r20, 0b11111111
-		STS tmr1_hamming_buffer, r20
-
-		INCW r17, r18
-		STS tmr1_counter + 0, r17
-		STS tmr1_counter + 1, r18
-
-		RJMP senddata_end
-
-	senddata_write_normal:
-		SETZ data_buffer
-		SUMZW r17, r18
-		LD r19, Z
-		WRITEBYTE r19, r18, r17, r16
-
-senddata_end:
-	POP r20
-	POP r19
-	POP r18
-	POP r17
-	POP r16
-	RET
-
-// byte can_write();
-can_write:
-	LDS r16, UCSR0A
-	ORI r16, 0b11011111
-	CPI r16, 0xFF
-	IN r16, SREG
-	LSR r16
-	ANDI r16, 0b00000001
-	RET
 
 // timer 0 interruption
 tmr0_start:
@@ -319,28 +181,3 @@ display_shield_end:
 display_shield_reset:
 	LDI r22, 0
 	RJMP display_shield_end
-
-// byte generate_pseudorandom_number();
-.DSEG
-	pseudorand_mem: .BYTE 1
-.CSEG
-generate_pseudorandom_number:
-	PUSH r17
-	LDS r16, pseudorand_mem
-
-	MOV r17, r16
-	LSL r17
-	LSL r17
-	LSL r17
-	EOR r16, r17
-	MOV r17, r16
-	LSR r17
-	LSR r17
-	LSR r17
-	LSR r17
-	LSR r17
-	EOR r16, r17
-
-	STS pseudorand_mem, r16
-	POP r17
-	RET
